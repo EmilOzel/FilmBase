d8da07bfc5f2fcbd9fd38942018172f13b75d655
EmilOzel <104371216+EmilOzel@users.noreply.github.com>
2025-11-17 09:55:38 +0100

    Initial application structure

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import filmbase.app.Filmbase;
+
+public class Main {
+    static void main() {
+        Filmbase filmbase = new Filmbase();
+        filmbase.start();
+    }
+}
---
+++ src/filmbase/app/Filmbase.java
@@ -0,0 +1,10 @@
+package filmbase.app;
+
+public class Filmbase {
+    public Filmbase() {
+    }
+
+    public void start(){
+        System.out.println("FilmBasen er startet");
+    }
+}
